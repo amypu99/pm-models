dfbb77f061b712fc4a3d06259a9aee9f5eeb4b8c
amypu99 <ap4489@columbia.edu>
2025-02-13 15:54:41 -0500

    test out different baseline outputs with closing reminder

--- classify/run_baseline.py
+++ classify/run_baseline.py
@@ -77,7 +77,7 @@ def run_pipeline(filepath):
     pipe.model = pipe.model.to('cuda')
 
 
-    for i in range(20):
+    for i in range(30):
         content = inference_df.Prompt.values[i] +  clean_text(inference_df.Context.values[i])
         input = tokenizer(content, max_length=20000, return_tensors='pt').to('cuda')
         decoded = tokenizer.decode(input["input_ids"][0][1:-1])
@@ -87,13 +87,36 @@ def run_pipeline(filepath):
         result = pipe(message, max_new_tokens=300, do_sample=False)
         with open(filepath, "a") as f:
             generated_text = result[0]["generated_text"]
-            f.write("\n\n\n<INSTRUCTION>\n\n\n")
-            f.write(generated_text[0]["content"])
+            # f.write("\n\n\n<INSTRUCTION>\n\n\n")
+            # f.write(generated_text[0]["content"])
             f.write("\n\n\n<RESPONSE>\n\n\n")
             f.write(generated_text[1]["content"])
-            f.write("\n\n\n<LABEL>\n\n\n")
-            f.write(inference_df.Response.values[i])
+            f.write(f"\n\n\n<LABEL>: {inference_df.Response.values[i]}")
 
-run_pipeline("pipeline_baseline_procedurally_barred.txt")
+# Run model with pipeline with closing reminder
+def run_pipeline_with_closing_reminder(filepath):
+
+    pipe = pipeline("text-generation", model=model, torch_dtype=torch.bfloat16, device='cuda', tokenizer=tokenizer)
+    pipe.model = pipe.model.to('cuda')
+
+
+    for i in range(30):
+        content = inference_df.Prompt.values[i] +  clean_text(inference_df.Context.values[i])
+        tokenized_content = tokenizer(content, max_length=18000, return_tensors='pt').to('cuda')
+        tokenized_content = tokenizer.decode(tokenized_content["input_ids"][0][1:-1])
+        tokenized_content = tokenized_content + """\n\nAbove is the appellate case. Read over the case carefully and remember that your job is to determine whether the case meets some criteria for further evaluation of prosecutorial misconduct. """
+        message = [
+            {"role": "user", "content": tokenized_content},
+            ]
+        result = pipe(message, max_new_tokens=300, do_sample=False)
+        with open(filepath, "a") as f:
+            generated_text = result[0]["generated_text"]
+            f.write("\n\n\n<RESPONSE>\n\n\n")
+            f.write(generated_text[1]["content"])
+            f.write(f"\n\n\n<LABEL>: {inference_df.Response.values[i]}")
+
+# run_pipeline("pipeline_baseline_procedurally_barred.txt")
+# run_pipeline("outputs_baseline_truncated.txt")
+run_pipeline_with_closing_reminder("outputs_baseline_closing_reminder.txt")
 # run_generate("generate_baseline.txt")
 

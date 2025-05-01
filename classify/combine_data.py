2e776a92fc287a1ce1f3c9e2d96837d7a64073be
amypu99 <ap4489@columbia.edu>
2025-05-01 00:16:25 -0400

    fix file

--- classify/combine_data.py
+++
@@ -1,9 +0,0 @@
-import os
-
-dir = "../cases_olmocr/MS/results/"
-with open("../cases_olmocr/MS/ms_olmocr_2.jsonl", "w") as out_f:
-    for file in os.listdir(dir):
-        with open(dir+file, "r") as in_f:
-            out_f.write(in_f.read())
-            out_f.write("\n")
-            
